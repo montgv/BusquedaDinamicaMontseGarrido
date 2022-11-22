ba4bded39fb897332525f160bb69ac7cf6660704
estudiante <montserrat.garrido.220@estudiante.iesoretania.es>
2022-11-22 14:28:52 +0100

    Agregar archivos de proyecto.

---
+++ BusquedaDinamicaMontseGarrido/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace BusquedaDinamicaMontseGarrido
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.tbBuscar = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(26, 112);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(735, 301);
+            this.dgv.TabIndex = 0;
+            // 
+            // tbBuscar
+            // 
+            this.tbBuscar.Location = new System.Drawing.Point(26, 49);
+            this.tbBuscar.Name = "tbBuscar";
+            this.tbBuscar.Size = new System.Drawing.Size(243, 20);
+            this.tbBuscar.TabIndex = 1;
+            this.tbBuscar.TextChanged += new System.EventHandler(this.tbBuscar_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbBuscar);
+            this.Controls.Add(this.dgv);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.TextBox tbBuscar;
+    }
+}
+
---
+++ BusquedaDinamicaMontseGarrido/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BusquedaDinamicaMontseGarrido
+{
+    public partial class Form1 : Form
+    {
+        string cadenaConexion = "Server = localhost; Database = Montse; User Id = sa; Password = oretania";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conectar();
+        }
+
+        public void conectar()
+        {
+            string sentencia = "SELECT * FROM ";
+
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                conexion.Open();
+                SqlCommand comando = new SqlCommand(sentencia, conexion);
+                SqlDataReader dataReader = comando.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(dataReader);
+                dgv.DataSource = dt;
+                comando.Dispose();
+                conexion.Close();
+
+            } catch (SqlException e)
+            {
+                MessageBox.Show("No se puede realizar la conexión.");
+            }
+        }
+
+        private void tbBuscar_TextChanged(object sender, EventArgs e)
+        {
+            consultarRowFilter();
+        }
+
+        private void consultarRowFilter()
+        {
+            string sentencia = "SELECT * FROM ";
+
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                conexion.Open();
+                SqlCommand comando = new SqlCommand(sentencia, conexion);
+                SqlDataReader dataReader = comando.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(dataReader);
+
+                dt.DefaultView.RowFilter = string.Format(" LIKE '{0}'", tbBuscar.Text);
+
+                dgv.DataSource = dt;
+                comando.Dispose();
+                conexion.Close();
+
+            }
+            catch (SqlException e)
+            {
+                MessageBox.Show("No se puede realizar la conexión del RowFilter.");
+            }
+        }
+    }
+}
