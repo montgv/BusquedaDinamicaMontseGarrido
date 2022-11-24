7c823c0860d9563f8d1b5f32db293cdc9f750bb8
Montse <montserrat.garrido.220@estudiante.iesoretania.es>
2022-11-24 22:15:55 +0100

    Error no pasa los datos

---
+++ BusquedaDinamicaMontseGarrido/FormPrincipal.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace BusquedaDinamicaMontseGarrido
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.tbBuscar = new System.Windows.Forms.TextBox();
+            this.lbTitulo = new System.Windows.Forms.Label();
+            this.cbColumna = new System.Windows.Forms.ComboBox();
+            this.lbBusqueda = new System.Windows.Forms.Label();
+            this.lbColumna = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(39, 172);
+            this.dgv.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersWidth = 62;
+            this.dgv.Size = new System.Drawing.Size(746, 347);
+            this.dgv.TabIndex = 0;
+            // 
+            // tbBuscar
+            // 
+            this.tbBuscar.Location = new System.Drawing.Point(39, 116);
+            this.tbBuscar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbBuscar.Name = "tbBuscar";
+            this.tbBuscar.Size = new System.Drawing.Size(362, 26);
+            this.tbBuscar.TabIndex = 1;
+            this.tbBuscar.TextChanged += new System.EventHandler(this.tbBuscar_TextChanged);
+            // 
+            // lbTitulo
+            // 
+            this.lbTitulo.AutoSize = true;
+            this.lbTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.lbTitulo.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitulo.Location = new System.Drawing.Point(199, 18);
+            this.lbTitulo.Name = "lbTitulo";
+            this.lbTitulo.Size = new System.Drawing.Size(388, 45);
+            this.lbTitulo.TabIndex = 2;
+            this.lbTitulo.Text = "Búsqueda dinámica DGV";
+            // 
+            // cbColumna
+            // 
+            this.cbColumna.FormattingEnabled = true;
+            this.cbColumna.Location = new System.Drawing.Point(517, 116);
+            this.cbColumna.Name = "cbColumna";
+            this.cbColumna.Size = new System.Drawing.Size(268, 28);
+            this.cbColumna.TabIndex = 3;
+            this.cbColumna.SelectedIndexChanged += new System.EventHandler(this.cbColumna_SelectedIndexChanged);
+            // 
+            // lbBusqueda
+            // 
+            this.lbBusqueda.AutoSize = true;
+            this.lbBusqueda.Location = new System.Drawing.Point(35, 82);
+            this.lbBusqueda.Name = "lbBusqueda";
+            this.lbBusqueda.Size = new System.Drawing.Size(191, 20);
+            this.lbBusqueda.TabIndex = 4;
+            this.lbBusqueda.Text = "Realiza aquí la búsqueda:";
+            // 
+            // lbColumna
+            // 
+            this.lbColumna.AutoSize = true;
+            this.lbColumna.Location = new System.Drawing.Point(513, 82);
+            this.lbColumna.Name = "lbColumna";
+            this.lbColumna.Size = new System.Drawing.Size(272, 20);
+            this.lbColumna.TabIndex = 5;
+            this.lbColumna.Text = "Selecciona la columna donde buscar:";
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 545);
+            this.Controls.Add(this.lbColumna);
+            this.Controls.Add(this.lbBusqueda);
+            this.Controls.Add(this.cbColumna);
+            this.Controls.Add(this.lbTitulo);
+            this.Controls.Add(this.tbBuscar);
+            this.Controls.Add(this.dgv);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FormPrincipal";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.TextBox tbBuscar;
+        private System.Windows.Forms.Label lbTitulo;
+        private System.Windows.Forms.ComboBox cbColumna;
+        private System.Windows.Forms.Label lbBusqueda;
+        private System.Windows.Forms.Label lbColumna;
+    }
+}
+
---
+++ BusquedaDinamicaMontseGarrido/FormPrincipal.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BusquedaDinamicaMontseGarrido
+{
+    public partial class FormPrincipal : Form
+    {
+        string cadenaConexion = "server=localhost; database=Montse; user id=sa; password=oretania";
+
+        public FormPrincipal()
+        {
+            InitializeComponent();
+
+            //Cambiamos el color de fondo, el color y el tipo de la letra al formulario
+            BackColor = Color.FromArgb(184, 240, 237);
+            ForeColor = Color.FromArgb(222, 120, 125);
+            Font = new Font("Comic Sans MS", 11, FontStyle.Bold);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conectar();
+
+            aniadirColumnasComboBox();
+
+            Estilos estilo = new Estilos(this.dgv);
+
+            //Introducimos los diferetentes estilos
+            estilo.definirFilas("#7adcd5", "#b8575e", "#50b4ad", "----------");
+            estilo.definirCabecera("#7adcd5", "#50b4ad");
+            estilo.definirFilarAlternas("#de787d", "#7adcd5", "**********");
+
+            //Ajustamos el tamño de todas las columnas
+            foreach (DataGridViewColumn dgvColumna in this.dgv.Columns)
+            {
+                dgvColumna.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            }
+        }
+
+        //Añadimos la columnas al comboBox
+        private void aniadirColumnasComboBox()
+        {
+            foreach (DataGridViewColumn columna in dgv.Columns)
+            {
+                cbColumna.Items.Add(columna.HeaderText);
+            }
+            cbColumna.SelectedIndex = 0;
+        }
+
+        //Conectamos con la base de datos
+        public void conectar()
+        {
+            string sentencia = "SELECT * FROM formulario";
+
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                conexion.Open();
+                SqlCommand comando = new SqlCommand(sentencia, conexion);
+                SqlDataReader dataReader = comando.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(dataReader);
+                dgv.DataSource = dt;
+                comando.Dispose();
+                conexion.Close();
+
+            } catch (SqlException e)
+            {
+                MessageBox.Show("No se puede realizar la conexión.");
+            }
+        }
+
+        //Evento que al cambiar el texto del textBox filtra por rowFilter
+        private void tbBuscar_TextChanged(object sender, EventArgs e)
+        {
+            consultarRowFilter();
+        }
+
+        //Filtro rowFilter
+        private void consultarRowFilter()
+        {
+            string sentencia = "SELECT * FROM formulario";
+
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                conexion.Open();
+                SqlCommand comando = new SqlCommand(sentencia, conexion);
+                SqlDataReader dataReader = comando.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(dataReader);
+
+                dt.DefaultView.RowFilter = string.Format(cbColumna.Text + " LIKE '{0}'", tbBuscar.Text);
+
+                dgv.DataSource = dt;
+                comando.Dispose();
+                conexion.Close();
+
+            }
+            catch (SqlException e)
+            {
+                MessageBox.Show("No se puede realizar la conexión del RowFilter.");
+            }
+        }
+
+        //Evento que al seleccionar un item del comboBox filtra por rowFilter
+        private void cbColumna_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            consultarRowFilter();
+        }
+    }
+}
